a84a7e3ecb7f6199598faa1b2f91d21a9e53ab2c
Jorge <jorgelmh_01@hotmail.com>
2020-05-14 05:03:30 +0100

    Creating my first bot

---
+++ bot.js
@@ -0,0 +1,36 @@
+/* Simple bot for Master and the boy's discord server */
+const Discord = require('discord.io')
+
+/* Import token */
+const {AuthToken, devGame} = require('./config.js')
+
+/* Instantiating the bot */
+const bot = new Discord.Client({
+    token: AuthToken,
+    autorun: true
+})
+
+/* message Listeners */
+bot.on('message', function(user, userID, channelID, message, event) {
+    if (message.substring(0, 1) === '!') {
+
+        let command = message.substring(1).split(' ')[0]
+
+        /* List of commands */
+        switch (command){
+            case 'gamedev':
+                bot.sendMessage({
+                    to: channelID,
+                    message: `Play the Lastest version of Goorton's web game :video_game: : ${devGame}`
+                })
+                break
+
+            case 'pinglan':
+                bot.sendMessage({
+                    to: channelID,
+                    message: `:computer: Press WINDOWS + R and run: 'ping 104.160.136.3'`
+                })
+                break
+        }
+    }
+});
---
+++ config.js
@@ -0,0 +1,19 @@
+/**
+ * ========================
+ *       Auth-Token
+ * ========================
+ */
+
+const AuthToken = 'NzEwMzI2NTY1Mjk2MTQ0NTA4.Xry2oQ.NQZtalx40cfnIQHwwMBlFGIRhW4'
+
+/**
+ * =========================
+ *      Browser Game
+ * =========================
+ */
+const devGame = 'https://tile-game-online.herokuapp.com/online'
+
+module.exports = {
+    AuthToken,
+    devGame
+}
